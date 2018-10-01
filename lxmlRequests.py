31308e9bcbc81afcf1ec9c9243446708cd831561
Ron F. del Rosario <ronsurf23@gmail.com>
2018-10-01 12:23:29 -0700

    Create lxmlRequests.py

---
+++ lxmlRequests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from lxml import html
+import requests
+
+page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
+tree = html.fromstring(page.content)
+
+#create a list of buyers
+buyers = tree.xpath('//div[@title="buyer-name"]/text()')
+#This will create a list of prices
+prices = tree.xpath('//span[@class="item-price"]/text()')
+
+print 'Buyers: ', buyers
+print 'Prices: ', prices
